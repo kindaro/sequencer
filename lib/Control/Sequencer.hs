a07c91645dbccbfee48a6b97705615f3c24e6dfd
Ignat Insarov <kindaro@gmail.com>
2019-08-10 20:34:37 +0300

    Refactor.

--- lib/Control/Sequencer.hs
+++ lib/Control/Sequencer.hs
@@ -1,8 +1,9 @@
 module Control.Sequencer
     ( independent
-    , independent'
     , insistent
     , redundant
+    , serialize
+    , trySynchronous
     , SequencingFailure
     ) where
 
@@ -11,22 +12,20 @@ import Control.Monad.Catch (MonadCatch)
 import Control.Monad.Writer.Strict (MonadWriter, tell)
 import Control.Applicative (Alternative, empty)
 import Data.List (genericReplicate)
-import Data.Sequence (Seq)
+import Control.Exception (displayException)
+import Data.Foldable (asum)
 
 import Control.Sequencer.Internal
 
--- | Run all the actions independently from each other, collect the results in a list and log the
--- errors in a sequence.
-independent :: forall a m ins. (MonadWriter (Seq SomeException) m, MonadCatch m, Traversable ins)
-            => ins (m a) -> m [a]
-independent = independent' (tell . pure)
+data SequencingFailure = SequencingFailure deriving Show
+instance Exception SequencingFailure where displayException _ = "sequencing failure"
 
 -- | Run all the actions independently from each other, collect the results and log the errors
 -- with the supplied logger.
-independent' :: forall a m e ins outs.
+independent :: forall a m e ins outs.
                     (Exception e, MonadCatch m, Traversable ins, Alternative outs)
-             => (e -> m ()) -> ins (m a) -> m (outs a)
-independent' logger = alternativeTranscode f
+            => (e -> m ()) -> ins (m a) -> m (outs a)
+independent logger = serialize f
     where f u = fmap pure u `catchSynchronous` \e -> logger e *> return empty
 
 insistent :: (MonadCatch m, MonadWriter (q SomeException) m, Alternative q)
@@ -36,3 +35,10 @@ insistent n = redundant . genericReplicate n
 redundant :: (MonadCatch m, MonadWriter (q SomeException) m, Foldable f, Alternative q)
           => f (m a) -> m a
 redundant = foldr1 f where f x y = x `catchSynchronous` (\e -> (tell . pure) e *> y)
+
+trySynchronous :: forall e m a. (Exception e, MonadCatch m) => m a -> m (Either e a)
+trySynchronous x = fmap Right x `catchSynchronous` (return . Left)
+
+serialize :: (Traversable ins, Alternative outs, Monad m)
+          => (m a -> m (outs b)) -> ins (m a) -> m (outs b)
+serialize f = fmap asum . sequence . fmap f
--- lib/Control/Sequencer/Internal.hs
+++ lib/Control/Sequencer/Internal.hs
@@ -1,20 +1,10 @@
-module Control.Sequencer.Internal where
+module Control.Sequencer.Internal
+    ( catchSynchronous
+    ) where
 
-import Control.Exception (Exception, SomeAsyncException, displayException)
 import Control.Monad.Catch (MonadCatch, Handler(..), throwM, catches)
-import Control.Applicative (Alternative)
-import Data.Foldable (asum)
-
-data SequencingFailure = SequencingFailure deriving Show
-instance Exception SequencingFailure where displayException _ = "sequencing failure"
-
-alternativeTranscode :: (Traversable ins, Alternative outs, Monad m)
-                     => (m a -> m (outs b)) -> ins (m a) -> m (outs b)
-alternativeTranscode f = fmap asum . sequence . fmap f
+import Control.Exception (Exception, SomeAsyncException)
 
 catchSynchronous :: forall e m a. (Exception e, MonadCatch m) => m a -> (e -> m a) -> m a
 catchSynchronous action handler = action `catches`
     [ Handler (throwM :: SomeAsyncException -> m w), Handler handler ]
-
-trySynchronous :: forall e m a. (Exception e, MonadCatch m) => m a -> m (Either e a)
-trySynchronous x = fmap Right x `catchSynchronous` (return . Left)
