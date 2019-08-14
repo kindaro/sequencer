ac7b1b6b59ec11a4387af251ca2ea54fa7f073d7
Ignat Insarov <kindaro@gmail.com>
2019-08-14 13:52:47 +0300

    Work in progress.

--- verify/Internal.hs
+++ verify/Internal.hs
@@ -4,7 +4,11 @@ import Control.Sequencer.Internal
 import Test.Tasty
 import Test.Tasty.HUnit
 
-checks = testGroup ""
-    [ testCase "" $ assertEqual "" [[1,2,3]] $ alternativeTranscode @[] @[] (fmap pure) [[1],[2],[3]]
+checks = testGroup "alternativeTranscode"
+    [ testCase "transpose a list"
+        $ assertEqual "" [[1,2,3]]
+            $ alternativeTranscode (fmap pure) [[1],[2],[3]]
+    , testCase "transpose any list"
+        $ undefined
     ]
 
--- verify/Main.hs
+++ verify/Main.hs
@@ -3,11 +3,13 @@ module Main where
 import Data.List (genericReplicate)
 
 import Control.Sequencer
-import Control.Sequencer.Internal
 import Test.Tasty
+import Test.Tasty.LeanCheck
+import Data.List
+import Control.Applicative
 
-import qualified Internal
-import qualified External
+-- import qualified Internal
+-- import qualified External
 
 main :: IO ()
 main = do
@@ -16,4 +18,13 @@ main = do
     --
     -- 1. Run `insistent` many times over randomly failing action.
 
-    defaultMain $ Internal.checks
+    defaultMain $
+
+    -- Laws:
+
+        testGroup "Laws of `serialize`:"
+            [ testProperty "serialize (fmap pure) == sequence -- with Maybe [Int]"
+                \mxs -> serialize (fmap pure) mxs == sequence @Maybe (mxs :: Maybe [Int])
+            , testProperty "serialize (fmap pure) == sequence -- with [Maybe Int]"
+                \mxs -> serialize (fmap pure) mxs == sequence @[] (mxs :: [Maybe Int])
+            ]
