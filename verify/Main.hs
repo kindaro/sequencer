6747fdcee25b7274c9dbd8fd196ab55b0f977c76
Ignat Insarov <kindaro@gmail.com>
2019-08-17 22:25:01 +0300

    Update check runner.

--- verify/Main.hs
+++ verify/Main.hs
@@ -4,9 +4,12 @@ import Test.Tasty
 import Test.Tasty.QuickCheck
 
 import Redundant
+import Assets
 
 
 main :: IO ()
-main = defaultMain $
-
-    testProperty "redundant can be retracted" prop_redundant_all
+main = defaultMain $ testGroup "Laws."
+    [ testProperty "example action can be retracted"
+        \x -> (retractExampleAction . fst . fst) (runM @Integer exampleAction [x]) == Just x
+    , testProperty "`redundant` can be retracted" prop_redundant_all
+    ]
